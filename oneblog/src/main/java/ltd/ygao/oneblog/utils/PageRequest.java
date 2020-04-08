97b86f372d9ee6f5a07d5bed2a442983ae1aea57
Kevin <ygao0928@outlook.com>
2020-04-08 13:56:29 +0800

    实现mybatis pagehelper分页后台功能

---
+++ oneblog/src/main/java/ltd/ygao/oneblog/utils/PageRequest.java
@@ -0,0 +1,20 @@
+package ltd.ygao.oneblog.utils;
+
+import lombok.Data;
+
+/**
+ * 分页请求
+ */
+@Data
+public class PageRequest {
+    /**
+     * 当前页码
+     */
+    private int pageNum;
+    /**
+     * 每页数量
+     */
+    private int pageSize;
+    
+
+}
